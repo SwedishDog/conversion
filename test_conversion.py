bff8add475cab090f408ad490f5aa255ed394998
SwedishDog <100093667+SwedishDog@users.noreply.github.com>
2022-02-20 17:07:24 -0600

    Update test_conversion.py

--- test_conversion.py
+++ test_conversion.py
@@ -2,12 +2,19 @@ import unittest
 from calculator import ConversionRate
 
 class TestCalc(unittest.TestCase):
-    def test_calc(self): 
+    def correct(self): 
         carcount = 100
         armwashes = 50
         planssold = 5
         result = ConversionRate(carcount, armwashes, planssold)
         self.assertEqual(result,10)
+
+    def incorrect(self):
+        carcount = 100
+        armwashes = 40
+        planssold = 10
+        result = ConversionRate(carcount, armwashes, planssold)
+        self.assertEqual(result, 50)
 
 if __name__ == '__main__':
     unittest.main()

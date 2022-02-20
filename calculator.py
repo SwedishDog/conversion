9b8d54fa7de22ff9f7a9895b2f90d9bdd082dcde
SwedishDog <100093667+SwedishDog@users.noreply.github.com>
2022-02-20 15:55:54 -0600

    uploading

---
+++ calculator.py
@@ -0,0 +1,15 @@
+##Total Car count minus ARM washes = Drive Up Washes
+##Divide plans sold by Drive Up Washes * 100 for Conversion Rate
+
+carCount = input("What was the carcount for the day? ")
+ARMWashes = input("How many of those cars were members? ")
+plansSold = input("How many plans did we sell today? ")
+
+def ConversionRate(carCount,ARMWashes,plansSold):
+    carCount = int(carCount)
+    ARMWashes = int(ARMWashes)
+    plansSold = int(plansSold)
+    rate = (plansSold/(carCount-ARMWashes))
+    print("The Conversion Rate is {:.2%}".format(rate))
+
+ConversionRate(carCount,ARMWashes,plansSold)
